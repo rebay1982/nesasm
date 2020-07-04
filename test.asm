7c479449f9fb90de2cacd9f9222fe8093682ddf0
Reda Baydoun <reda.baydoun@gmail.com>
2020-07-04 11:18:54 -0400

    feat: fix formatting

--- test.asm
+++ test.asm
@@ -3,23 +3,23 @@
   .inesmap 0   ; mapper 0 = NROM, no bank swapping
   .inesmir 1   ; background mirroring (ignore for now)
 
-	.bank 0
-	.org $C000
+  .bank 0
+  .org $C000
 reset:
-	.include "reset.asm"
+  .include "reset.asm"
 
 main:
-	LDA	#%10000000		; Bit 7 of PPU register at $2001 intensifies blue.
-	STA $2001
-	JMP main
+  LDA #%10000000
+  STA $2001
+  JMP main
 
 nmi:
-	.include "nmi.asm"
+  .include "nmi.asm"
 	
 
-	.bank 1
-	.org $FFFA
+  .bank 1
+  .org $FFFA
 interrupt_vector:
-	.dw nmi
-	.dw	reset
-	.dw 0
+  .dw nmi
+  .dw	reset
+  .dw 0
