80c9df5ec065cb6cca177bae4319c476968014fc
Reda Baydoun <reda.baydoun@gmail.com>
2020-08-14 01:02:53 -0400

    feat(pong): Start a pong example

--- game.asm
+++ game.asm
@@ -23,7 +23,120 @@ update_title_return:
 
 
 update_game:
+	JSR update_ball_dir
 
+	JSR move_ball
 
 update_game_return:
 	RTS
+
+
+
+;=============================
+; move_ball
+;=============================
+; Routine dedicated to move
+; the first sprite  based on
+; ball_dir.
+;=============================
+; Can be made better by
+; making $0200 and $0203 
+; parameters.
+;=============================
+move_ball:
+move_horizontal:
+	LDA #%00000010
+	AND ball_dir
+
+	BNE move_right
+move_left
+	DEC $0203                  ; byte 3 is X position.
+	JMP move_vertical
+
+move_right:
+	INC $0203                  ; byte 3 is X position.
+
+move_vertical:
+	LDA #%00000001
+	AND ball_dir
+
+	BNE move_down
+move_up:
+	DEC $0200                  ; byte 0 is Y position.
+	JMP move_ball_return
+
+move_down:
+	INC $0200                  ; byte 0 is Y position.
+
+move_ball_return:
+	RTS
+
+
+
+;=============================
+; update_ball_dir
+;=============================
+; Routine to update the ball's
+; direction based on its
+; current position.
+;=============================
+; When we hit screen top, set
+;   bit 0 of ball_dir to 1.
+; When we hit screen bottom,
+;  set bit 0 of ball_dir to 0.
+; When we hit screen left, set
+;  bit 1 if ball_dir to 1.
+; When we hit screen right,
+;  set bit 1 of ball_dir to 0.
+;=============================
+; + Performance could be
+;   better if registers were
+;   better used instead of
+;   always loading into
+;   accumulator.
+; + Use parameters instead
+;   of $0200 and $0203
+;=============================
+update_ball_dir:
+check_top:
+	LDA $0200
+	CMP #wall_top
+	BNE check_bottom
+
+	LDA ball_dir
+	ORA #$01
+	STA ball_dir
+
+	JMP check_left             ; If top is eq to 0, no need to check bottom.
+
+check_bottom:
+	CMP #wall_bottom
+	BNE check_left
+
+	LDA ball_dir
+	AND #%11111110
+	STA ball_dir
+
+check_left:
+	LDA $0203                  ; X pos is sprite attribute data's 4th byte. (byte 3)
+	CMP #wall_left
+	BNE check_right
+
+	LDA ball_dir
+	ORA #$02
+	STA ball_dir
+
+	JMP check_done
+
+check_right:
+	CMP #wall_right
+	BNE check_done
+
+	LDA ball_dir
+	AND #%11111101
+	STA ball_dir
+
+check_done:
+	RTS
+
+
--- main.asm
+++ main.asm
@@ -3,11 +3,20 @@
 	.inesmap 0                 ; mapper 0 = NROM, no bank swapping
 	.inesmir 1                 ; background mirroring (ignore for now)
 
+;=============================
+; CONSTANTS
+;============================
 bg_draw_buffer = $0300
+wall_top = $08               ; NTSC NES doesn't render the first 8pixel row to screen.
+wall_bottom = $DF            ; 240 - 8, -8 (last 8 rows not shown on screen on NTSC NES)
+wall_right = $F8             ; Decimal 248 (256 - 8) 
+wall_left = $00
 
+
+;=============================
+; VARIABLES
+;=============================
 	.rsset $0000               ; Start data at memory address 0.
-score_1 .rs 1
-score_2 .rs 1
 buttons_1 .rs 1              ; Buttons for controller 1.
 buttons_2 .rs 2              ; Buttons for controller 2.
 state .rs 1                  ; Contains the game state.
@@ -17,8 +26,7 @@ sleep .rs 1                  ; Sleep state counter.
 request_dma .rs 1            ; Request DMA transfer for sprites.
 request_draw .rs 1           ; Request that the PPU draws during vblank.
 
-sprite_pos_x .rs 1           ; Sprite X coordinate
-sprite_pos_y .rs 1           ; Sprite Y coordinate
+ball_dir .rs 1               ; Defines in which direction the ball should be going.
 
 ; This has to be in zero page
 params .rs 32
@@ -53,14 +61,14 @@ setup_palettes_loop:
 	CPX #$20                   ; Check if X == 32
 	BNE setup_palettes_loop
 
-;setup_sprite_data:
-;	LDX #$00
-;setup_sprite_data_loop:
-;	LDA sprite_attr_data, x
-;	STA $0200, x
-;	INX
-;	CPX #$04
-;	BNE setup_sprite_data_loop
+setup_sprite_data:
+	LDX #$00
+setup_sprite_data_loop:
+	LDA sprite_attr_data, x
+	STA $0200, x
+	INX
+	CPX #$04
+	BNE setup_sprite_data_loop
 
 setup_bg_attr:
 	LDA $2002
@@ -137,17 +145,17 @@ sleep_loop:
 	.org $E000
 
 palette_data:
-	.db $0F, $20, $01, $00     ;background palette data
+	.db $22, $20, $01, $00     ;background palette data
 	.db $22, $20, $01, $00
 	.db $22, $20, $01, $00
 	.db $22, $20, $01, $00
-	.db $0F, $1C, $15, $14     ;sprite palette data 
+	.db $22, $1C, $15, $14     ;sprite palette data
 	.db $22, $02, $38, $3C
 	.db $22, $1C, $15, $14
 	.db $22, $02, $38, $3C 
 
 sprite_attr_data:
-	.db $80, $75, $00, $80     ; Sprite 1
+	.db wall_top, $75, $00, wall_left     ; Sprite 1
 
 title_screen_data:
 	.db $24, $24, $24, $24
